54b65286b38b834716e4f5641fa06aee8edaa6b6
tvillari <tvillari@leafnoise.io>
2024-03-20 21:16:04 -0300

    clase 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleComponent } from './example/example.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StudentsPageComponent } from './students-page/students-page.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent,
+    ToolbarComponent,
+    StudentsPageComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/example/example.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrl: './example.component.css'
+})
+export class ExampleComponent {
+
+}
